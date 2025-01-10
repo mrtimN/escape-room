dc50d9dcadd99da2e742eaa4e077d25ae1c15701
mrtimN <t.niewolinski@gmail.com>
2025-01-10 01:29:58 +0100

    objects minified

--- objects.py
+++ objects.py
@@ -1,126 +1,30 @@
 # define rooms and items
+couch = {"name": "couch", "type": "furniture",}
+piano = {"name": "piano", "type": "furniture",}
+queen_bed = {"name": "queen bed", "type": "furniture",}
+double_bed = {"name": "double bed","type": "furniture",}
+dresser = {"name": "dresser","type": "furniture",}
+dining_table = {"name": "dining table","type": "furniture",}
+poster_1 = {"name": "poster 1","type": "furniture",}
+poster_2 = {"name": "poster 2","type": "furniture",}
+note_on_the_floor = {"name": "note on the floor","type": "furniture",}
+poster_4 = {"name": "poster 4","type": "furniture",}
 
-couch = {
-    "name": "couch",
-    "type": "furniture",
-}
+door_a = {"name": "door a","type": "door",}
+door_b = {"name": "door b","type": "door",}
+door_c = {"name": "door c","type": "door",}
+door_d = {"name": "door d","type": "door",}
 
-piano = {
-    "name": "piano",
-    "type": "furniture",
-}
+key_a = {"name": "key for door a","type": "key","target": door_a,}
+key_b = {"name": "key for door b","type": "key","target": door_b,}
+key_c = {"name": "key for door c","type": "key","target": door_c,}
+key_d = {"name": "key for door d","type": "key","target": door_d,}
 
-queen_bed = {
-    "name": "queen bed",
-    "type": "furniture",
-}
-
-double_bed = {
-    "name": "double bed",
-    "type": "furniture",
-}
-
-dresser = {
-    "name": "dresser",
-    "type": "furniture",
-}
-
-dining_table = {
-    "name": "dining table",
-    "type": "furniture",
-}
-
-poster_1 = {
-  "name": "poster 1",
-  "type": "furniture",
-}
-
-poster_2 = {
-  "name": "poster 2",
-  "type": "furniture",
-}
-
-note_on_the_floor = {
-  "name": "note on the floor",
-  "type": "furniture",
-}
-
-poster_4 = {
-  "name": "poster 4",
-  "type": "furniture",
-}
-
-door_a = {
-    "name": "door a",
-    "type": "door",
-}
-
-door_b = {
-    "name": "door b",
-    "type": "door",
-}
-
-door_c = {
-    "name": "door c",
-    "type": "door",
-}
-
-door_d = {
-    "name": "door d",
-    "type": "door",
-}
-
-key_a = {
-    "name": "key for door a",
-    "type": "key",
-    "target": door_a,
-}
-
-key_b = {
-    "name": "key for door b",
-    "type": "key",
-    "target": door_b,
-}
-
-key_c = {
-    "name": "key for door c",
-    "type": "key",
-    "target": door_c,
-}
-
-key_d = {
-    "name": "key for door d",
-    "type": "key",
-    "target": door_d,
-}
-
-game_room = {
-    "name": "game room",
-    "type": "room",
-    "explored" : False,
-}
-
-bedroom_1 = {
-    "name": "bedroom 1",
-    "type": "room",
-    "explored" : False,
-}
-
-bedroom_2 = {
-    "name": "bedroom 2",
-    "type": "room",
-    "explored" : False,
-}
-
-living_room = {
-    "name": "living room",
-    "type": "room",
-    "explored" : False,
-}
-
-outside = {
-  "name": "outside"
-}
+game_room = {"name": "game room","type": "room","explored" : False,}
+bedroom_1 = {"name": "bedroom 1","type": "room","explored" : False,}
+bedroom_2 = {"name": "bedroom 2","type": "room","explored" : False,}
+living_room = {"name": "living room","type": "room","explored" : False,}
+outside = {"name": "outside"}
 
 all_furniture = [couch, piano, queen_bed, double_bed, dresser, dining_table, poster_1]
 
